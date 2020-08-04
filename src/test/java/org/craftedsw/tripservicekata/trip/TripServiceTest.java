945cd5c683c70c052e49b64bdb113eb88f9ef4f9
Tom <thomas.spencer@codurance.com>
2020-08-04 17:12:51 +0100

    Add assertThrows

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,11 +1,17 @@
 package org.craftedsw.tripservicekata.trip;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.junit.jupiter.api.Test;
 
 public class TripServiceTest {
 
   @Test
   void should_throw_an_exception_when_user_is_not_logged_in() {
-
+    TripService tripService = new TripService();
+    assertThrows(UserNotLoggedInException.class, () -> {
+      tripService.getTripsByUser(null);
+    });
   }
 }
