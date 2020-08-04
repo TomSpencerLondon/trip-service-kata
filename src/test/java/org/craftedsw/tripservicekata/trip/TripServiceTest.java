8219b43f3ca049a2d6d561b82493e4036a5f4c27
Tom <thomas.spencer@codurance.com>
2020-08-04 17:16:01 +0100

    Add TestableTripService

--- src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -3,15 +3,24 @@ package org.craftedsw.tripservicekata.trip;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
 import org.junit.jupiter.api.Test;
 
 public class TripServiceTest {
 
   @Test
   void should_throw_an_exception_when_user_is_not_logged_in() {
-    TripService tripService = new TripService();
+    TripService tripService = new TestableTripService();
     assertThrows(UserNotLoggedInException.class, () -> {
       tripService.getTripsByUser(null);
     });
+  }
+
+  private class TestableTripService extends TripService {
+
+    @Override
+    protected User getLoggedInUser() {
+      return null;
+    }
   }
 }
