575e97448f587cf463ef7f578a8b4f9ae8a32368
Tom <thomas.spencer@codurance.com>
2020-08-04 17:07:44 +0100

    Initial commit

---
+++ src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -0,0 +1,11 @@
+package org.craftedsw.tripservicekata.trip;
+
+import org.junit.jupiter.api.Test;
+
+public class TripServiceTest {
+
+  @Test
+  void should_throw_an_exception_when_user_is_not_logged_in() {
+
+  }
+}
