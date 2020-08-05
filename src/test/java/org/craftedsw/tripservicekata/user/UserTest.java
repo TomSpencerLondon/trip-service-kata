6de1f17b24540d1607980058106064b3406904e5
Tom <thomas.spencer@codurance.com>
2020-08-05 15:33:57 +0100

    Add isFriendsWith

--- src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
+++ src/test/java/org/craftedsw/tripservicekata/user/UserTest.java
@@ -1,5 +1,22 @@
 package org.craftedsw.tripservicekata.user;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import org.craftedsw.tripservicekata.trip.UserBuilder;
+import org.junit.jupiter.api.Test;
+
 public class UserTest {
 
+  private static final User BOB = new User();
+  private static final User PAUL = new User();
+
+  @Test
+  void should_inform_when_users_are_not_friends() {
+    User user = UserBuilder.aUser()
+        .friendsWith(BOB)
+        .build();
+
+    assertThat(user.isFriendsWith(PAUL), is(false));
+  }
 }
